ee906b4778023ccc63e74299f045e50a32ecf5b7
Ujjawal <ujjawalkashyap1020@gstedge.com>
2018-06-16 18:57:20 +0530

    test

--- payload.manager.php
+++ payload.manager.php
@@ -3,10 +3,9 @@
   class payloadManager{
 
     public function manage($payload = ''){
-
-
       $insert = (new webhook_primary())->insert(payload::$payload);
       payload::$payload = array_merge(['webhook_id' => connect::$conn->insert_id],payload::$payload);
+
       $data = payload::$payload;
       if(in_array('payment', $data['contains']))
       $insert = (new webhook_payment())->insert($data);
